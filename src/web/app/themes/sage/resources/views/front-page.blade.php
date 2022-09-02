0b48954aa8eb2a5d3ce747dc7551047b09b04ff8
mahmoud <awad25.ma@gmail.com>
2022-09-02 14:51:14 +0000

    home page init

--- src/web/app/themes/sage/resources/views/front-page.blade.php
+++ src/web/app/themes/sage/resources/views/front-page.blade.php
@@ -3,8 +3,8 @@
 @endphp
 @section('content')
   @while(have_posts()) @php(the_post())
-  <div class="h-screen w-screen flex justify-center items-center">
-    <div class="bg-slate-600 rounded-md py-4 px-12 text-white text-center text-lg font-medium">
+  <div class="h-[50vh] w-screen flex justify-center items-center">
+    <div class="bg-primary rounded-md py-4 px-12 text-white text-center text-lg font-medium">
       {{$title}}
     </div>
   </div>

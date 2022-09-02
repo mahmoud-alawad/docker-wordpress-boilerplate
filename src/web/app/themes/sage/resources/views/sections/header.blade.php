9e124736ba8a9d68b8e840b7070ad3b0b181d634
mahmoud <awad25.ma@gmail.com>
2022-09-02 17:20:10 +0000

    WIP added header logo and menu

--- src/web/app/themes/sage/resources/views/sections/header.blade.php
+++ src/web/app/themes/sage/resources/views/sections/header.blade.php
@@ -1,11 +1,27 @@
+@php
+$logo = get_field('logo','option')?? [];
+@endphp
 <header class="bg-primary text-white fixed top-0 w-full " x-data="{menuOpen: false}">
   <div class="container flex justify-between items-center">
-    <a class="w-1/12 h-full flex items-center justify-center" href="{{ home_url('/') }}">
+    <a class="w-1/12 h-full flex flex-col items-center justify-center py-1 uppercase" href="{{ home_url('/') }}">
+      @if($logo?? false)
+      <img class="w-full max-h-12 object-cover"src="{{$logo['url']}}" alt="">
+      @endif
       {!! $siteName !!}
     </a>
-  
+    <nav class="nav-primary" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
+      {!! wp_nav_menu([
+        'theme_location' => 'primary_navigation',
+       'menu_class' => 'hidden lg:flex justify-end my-2', 
+       'echo' => false,
+       'add_li_classes' => 'px-2 font-bold opacity-75 text-md capitalize hover:opacity-100'
+       ]) !!}
+    </nav>
     @if (has_nav_menu('primary_navigation'))
-    <div data-role="nav-toggle" class="w-8 h-full hover:cursor-pointer z-50" @click="menuOpen = !menuOpen">
+    <div data-role="nav-toggle" class="w-8 h-full hover:cursor-pointer z-50 lg:hidden" 
+     @click="menuOpen = !menuOpen"
+     @keydown.escape.window="menuOpen = false"
+     >
       <div class=" text-[2rem] w-full h-full" :class="menuOpen ? 'hidden' : ''"><i class="fa-solid fa-bars"></i></div>
       <span class="icon-close text-[2rem]" :class="menuOpen ? '' : 'hidden'"><i class="fa-solid fa-close"></i></span>
     </div>
@@ -16,14 +32,17 @@
     x-transition:enter-end="translate-x-0"
     x-transition:leave-start="translate-x-0"
     x-transition:leave-end="translate-x-full"
-    x-transition:leave="ease-out duration-400"
-    class="absolute top-0 right-0 z-40 w-screen max-w-[80%] h-screen bg-primary">
+    x-transition:leave="ease-out duration-400" 
+    @mousedown.outside="menuOpen = false" //TODO click outside
+
+    class="absolute lg:hidden top-0 right-0 z-40 w-screen max-w-[80%] h-screen bg-primary drop-shadow-2xl">
+    <div class="px-6 pt-6 uppercase text-3xl">{{_('go to')}}</div>
       <nav class="nav-primary" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
         {!! wp_nav_menu([
           'theme_location' => 'primary_navigation',
          'menu_class' => 'mt-10 ml-6', 
          'echo' => false,
-         'add_li_classes' => 'py-2 font-bold text-2xl uppercase'
+         'add_li_classes' => 'py-2 font-medium text-2xl uppercase'
          ]) !!}
       </nav>
     </div>
